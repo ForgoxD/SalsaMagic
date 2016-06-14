c88163b76f00578fde8cbf8e7d962618df30cbc2
ForgoxD <forgoxd@gmail.com>
2016-06-14 22:40:42 +0200

    bla 2

---
+++ src/com/salsa/webScrapping/Neko.java
@@ -0,0 +1,12 @@
+package com.salsa.webScrapping;
+
+import java.util.ArrayList;
+
+import com.salsa.card.Card;
+
+public class Neko {
+	public static ArrayList<Card> parse(){
+		
+		return null;
+	}
+}
